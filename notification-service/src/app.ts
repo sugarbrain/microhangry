81eeaa40b60faecd61ecdc5295c2a38ab48a3ee4
aacgn <4augusto@gmail.com>
2019-06-16 17:23:53 -0300

    Create docker and folder structure for notification service

---
+++ notification-service/src/app.ts
@@ -0,0 +1,34 @@
+import 'reflect-metadata';
+import DatabaseConfig from './config/database';
+import * as Express from 'express';
+import * as bodyParser from 'body-parser';
+
+class Notification {
+    public app: Express.Application;
+    public PORT = '8081';
+
+    constructor() {
+        this.app = Express();
+        this._config();
+    }
+
+    public async start() {
+        console.log(`Notification SERVICE started.`);
+
+        this.app.listen(this.PORT, () => {
+            console.log(`Server listening in http://localhost:${this.PORT}`);
+        });
+    }
+
+    private async _config(): Promise<void> {
+        // Connecting to database
+        await DatabaseConfig.connect();
+
+        // Allowing body parser JSON
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+
+    }
+}
+
+new Notification().start();
