3d840b65f0d2ac7f12c6bcd2768c873f2792c056
mmps <mmps@cin.ufpe.br>
2019-06-25 22:33:33 +0000

    [CI/CD] Deploy services to heroku

--- notification-service/ormconfig.js
+++ notification-service/ormconfig.js
@@ -1,15 +1,14 @@
 module.exports = {
     "type": "postgres",
-    "host": process.env.DATABASE_HOST || "preference-db",
+    "host": process.env.DATABASE_HOST || "notification-db",
     "port": process.env.DATABASE_PORT || 5432,
     "username": process.env.DATABASE_USER || "postgres",
     "password": process.env.DATABASE_PASS || "postgres",
-    "database": process.env.DATABASE_NAME || "preference",
+    "database": process.env.DATABASE_NAME || "notification",
     "entities": ["src/entities/*.ts"],
     "migrations": ["src/database/migration/*.ts"],
     "cli": {
         "entitiesDir": "src/entities",
         "migrationsDir": "./src/database/migration"
-    },
-    "migrationRun": true
+    }
 }
