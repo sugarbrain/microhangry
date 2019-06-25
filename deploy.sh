3014f8576db926ba950cab9d0aa29f73ed8d557f
mmps <mmps@cin.ufpe.br>
2019-06-25 22:54:55 +0000

    Fix migrations on deploy

--- deploy.sh
+++ deploy.sh
@@ -11,20 +11,20 @@ docker login -u _ -p "$HEROKU_TOKEN"  registry.heroku.com
 
 docker tag microhangry_core-api registry.heroku.com/microhangry-api/web
 docker push registry.heroku.com/microhangry-api/web
-docker run registry.heroku.com/microhangry-api/web npm run typeorm -- migration:run
+heroku run -a microhangry-api npm run typeorm -- migration:run
 heroku container:release web -a microhangry-api
 
 docker tag microhangry_notification-service registry.heroku.com/microhangry-notification/web
 docker push registry.heroku.com/microhangry-notification/web
-docker run registry.heroku.com/microhangry-notification/web npm run typeorm -- migration:run
+heroku run -a microhangry-notification npm run typeorm -- migration:run
 heroku container:release web -a microhangry-notification
 
 docker tag microhangry_order-service registry.heroku.com/microhangry-order/web
 docker push registry.heroku.com/microhangry-order/web
-docker run registry.heroku.com/microhangry-order/web npm run typeorm -- migration:run
+heroku run -a microhangry-order npm run typeorm -- migration:run
 heroku container:release web -a microhangry-order
 
 docker tag microhangry_preference-service registry.heroku.com/microhangry-preference/web
 docker push registry.heroku.com/microhangry-preference/web
-docker run registry.heroku.com/microhangry-preference/web npm run typeorm -- migration:run
+heroku run -a microhangry-preference npm run typeorm -- migration:run
 heroku container:release web -a microhangry-preference
